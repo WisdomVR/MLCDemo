ee3f7557e197897e1a28bc64fdc17dcdba6cb21a
Hellen-Adua <155617576+Hellen-Adua@users.noreply.github.com>
2024-06-16 20:35:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-import streamlit as st
-import joblib
-import numpy as np
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.pipeline import Pipeline
+from sklearn.decomposition import PCA
+from imblearn.over_sampling import SMOTE
+from imblearn.pipeline import make_pipeline as imbalanced_make_pipeline
+import joblib
 import os
 
 # Function to load the model
